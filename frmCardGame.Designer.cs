b3dfa0a5db8a00d20f82faef69df05e15d2225a9
ChihYu0831 <chyulola434@gmail.com>
2026-05-20 23:07:40 +0800

    新增遊戲關卡選擇面板與按鈕
    在表單中新增 `pnlTitle` 面板，並加入三個按鈕：
    - `btnLevel1`：第一關 (4x4)
    - `btnLevel2`：第二關 (6x6)
    - `btnLevel3`：第三關 (8x8)
    將面板設定為頂部佈局，並更新初始化與釋放邏輯。

--- frmCardGame.Designer.cs
+++ frmCardGame.Designer.cs
@@ -28,21 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnlTitle = new System.Windows.Forms.Panel();
+            this.btnLevel1 = new System.Windows.Forms.Button();
+            this.btnLevel2 = new System.Windows.Forms.Button();
+            this.btnLevel3 = new System.Windows.Forms.Button();
+            this.pnlTitle.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pnlTitle
+            // 
+            this.pnlTitle.Controls.Add(this.btnLevel3);
+            this.pnlTitle.Controls.Add(this.btnLevel2);
+            this.pnlTitle.Controls.Add(this.btnLevel1);
+            this.pnlTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTitle.Location = new System.Drawing.Point(0, 0);
+            this.pnlTitle.Name = "pnlTitle";
+            this.pnlTitle.Size = new System.Drawing.Size(800, 105);
+            this.pnlTitle.TabIndex = 0;
+            // 
+            // btnLevel1
+            // 
+            this.btnLevel1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnLevel1.Location = new System.Drawing.Point(59, 38);
+            this.btnLevel1.Name = "btnLevel1";
+            this.btnLevel1.Size = new System.Drawing.Size(121, 32);
+            this.btnLevel1.TabIndex = 0;
+            this.btnLevel1.Text = "第一關 (4x4)";
+            this.btnLevel1.UseVisualStyleBackColor = true;
+            // 
+            // btnLevel2
+            // 
+            this.btnLevel2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnLevel2.Location = new System.Drawing.Point(186, 38);
+            this.btnLevel2.Name = "btnLevel2";
+            this.btnLevel2.Size = new System.Drawing.Size(121, 32);
+            this.btnLevel2.TabIndex = 1;
+            this.btnLevel2.Text = "第二關 (6x6)";
+            this.btnLevel2.UseVisualStyleBackColor = true;
+            // 
+            // btnLevel3
+            // 
+            this.btnLevel3.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnLevel3.Location = new System.Drawing.Point(350, 38);
+            this.btnLevel3.Name = "btnLevel3";
+            this.btnLevel3.Size = new System.Drawing.Size(121, 32);
+            this.btnLevel3.TabIndex = 2;
+            this.btnLevel3.Text = "第三關 (8x8)";
+            this.btnLevel3.UseVisualStyleBackColor = true;
             // 
             // frmCardGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlTitle);
             this.Name = "frmCardGame";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "棋牌類遊戲";
+            this.pnlTitle.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlTitle;
+        private System.Windows.Forms.Button btnLevel3;
+        private System.Windows.Forms.Button btnLevel2;
+        private System.Windows.Forms.Button btnLevel1;
     }
 }
 
